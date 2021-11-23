4447f3a64b3ae4901e887be5d86b50905013ad15
Try Hutomo Putra <try@Trys-MacBook-Pro.local>
2021-11-23 15:26:17 +0700

    first commit

---
+++ data.js
@@ -0,0 +1,77 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-interior-ectic",
+      "name": "Interior-Ectic",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 320,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.24815626582828187,
+          "pitch": -0.10386175727865776,
+          "title": "Kitchen Set",
+          "text": "Bahan HPL"
+        },
+        {
+          "yaw": 2.022971333281247,
+          "pitch": 0.0014797007489235625,
+          "title": "Kulkas Mahal",
+          "text": "Dua pintu, 1 colokan"
+        },
+        {
+          "yaw": -0.39622958664119423,
+          "pitch": 0.23069750178676074,
+          "title": "Kompor Cosmos",
+          "text": "Gas 3kg"
+        },
+        {
+          "yaw": 1.6105020323892045,
+          "pitch": 0.34286542489327587,
+          "title": "Magicom Sankei",
+          "text": "Masak nasi tutung"
+        },
+        {
+          "yaw": -1.2806304819643906,
+          "pitch": -0.39134964926152094,
+          "title": "Lampu Bohlam",
+          "text": "Sering ngajepret"
+        },
+        {
+          "yaw": -1.2999040067997125,
+          "pitch": 0.3943387704476571,
+          "title": "Minibar",
+          "text": "Kayu Jatibening"
+        },
+        {
+          "yaw": 0.5050819914567164,
+          "pitch": 0.26280518109098594,
+          "title": "Wastafel",
+          "text": "Bocor wae"
+        }
+      ]
+    }
+  ],
+  "name": "Interior-Ectic",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
